d56d27f39ff2697bbfe86e1f88e8805c846a00e9
PoornaSyntax <poornasairam09@gmail.com>
2024-10-20 09:33:40 -0400

    SQL Project commit

---
+++ src/main/java/org/practice/class01/E4SQL.java
@@ -0,0 +1,18 @@
+package org.practice.class01;
+import java.util.List;
+import java.util.Map;
+
+public class E4SQL {
+    public static void main(String[] args) {
+
+        List<Map<String,String>> data= DbUtils.fetch("Select * from employee");
+
+
+        System.out.println(data);
+        System.out.println(data.get(3));
+    Map<String,String> firstRowData= data.get(0);
+        System.out.println(firstRowData.get("salary"));
+    // Break till 7:45
+
+}
+}
